af3449084eb1abe7fcb56cdf78422bdfc401cb39
iv <91138367+Nox-iv@users.noreply.github.com>
2022-03-15 19:10:03 +0000

    headUpdate
    Bootstrap cdn update
    Old css link removed
    jquery cdn added
    jqueryui cdn added
    Formatted

--- view/_inc/head.php
+++ view/_inc/head.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang = "en">
   <head>
-  <script src="https://use.fontawesome.com/4f7fac2b4f.js"></script>
-    <title>Performance Tracker</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="site.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://use.fontawesome.com/4f7fac2b4f.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-bootstrap/0.5pre/assets/css/bootstrap.min.css">
     <link rel="stylesheet" type = "text/css" href = "../assets/css/Style.css">
     <link rel="stylesheet" type = "text/css" href = "../assets/css/FooterStyle.css">
     <link rel="stylesheet" type = "text/css" href = "../assets/css/HeaderStyle.css">
