2f1354842c91b19d150fdd498845f522e9874de1
iv <91138367+Nox-iv@users.noreply.github.com>
2022-03-19 17:34:33 +0000

    dbConnScript

---
+++ controller/DBConn.php
@@ -0,0 +1,24 @@
+<?php
+if(!isset($GLOBALS['DBConn'])){
+    connect();
+} else {
+    try {
+        $listdatabases = new MongoDB\Driver\Command(["listCollections" => 1]);
+        $res = $GLOBALS['DBConn']->executeCommand("projectDB", $listdatabases);
+    }
+    catch (Throwable $e){
+        connect();
+    }
+}
+
+function connect() {
+    try {
+        $m = new MongoDB\Driver\Manager('mongodb+srv://group1:fvAIyyCRp4PBaDPQ@clst01.to6hh.mongodb.net/projectDB?retryWrites=true&w=majority');
+        $GLOBALS['DBConnStatus'] = true;
+        $GLOBALS['DBConn'] = $m;
+    }
+    catch (Throwable $e) {
+        $GLOBALS['DBConnStatus'] = false;
+    }
+}
+?>
---
+++ controller/teamManagement.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+include 'DBConn.php';
+
+echo($GLOBALS['DBConnStatus']);
+
+?>
