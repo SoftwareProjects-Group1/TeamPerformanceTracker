087652eaa01f624793ee59f3791e4e77119cb078
iv <91138367+Nox-iv@users.noreply.github.com>
2022-03-19 19:02:19 +0000

    teamManagementDBLink
    Team management page updated to use data from the DB

--- controller/teamManagement.php
+++ controller/teamManagement.php
@@ -1,7 +1,30 @@
 <?php
 session_start();
 include 'DBConn.php';
+use MongoDB\Driver as Mongo;
+$CONN;
+if(isset($GLOBALS['DBConnStatus']) && $GLOBALS['DBConnStatus']==true){
+    $CONN=$GLOBALS['DBConn'];
+}
 
-echo($GLOBALS['DBConnStatus']);
+if(isset($_POST) && (isset($_POST['action']) && $_POST['action']=='getData')){
+    $teams=[];
+    $employees=[];
+    $projects=[];
+    $q = new Mongo\Query([],[]);
+    $cursor = $CONN->executeQuery('projectDB.Teams',$q);
+    foreach($cursor as $team) {
+        $teams[]=$team;
+    }
+    $cursor = $CONN->executeQuery('projectDB.Employees',$q);
+    foreach($cursor as $team) {
+        $employees[]=$team;
+    }
+    $cursor = $CONN->executeQuery('projectDB.Projects',$q);
+    foreach($cursor as $team) {
+        $projects[]=$team;
+    }
+    echo json_encode(["teams"=>$teams,"employees"=>$employees,"projects"=>$projects]);
+}
 
 ?>
--- view/scripts/teamManagement.js
+++ view/scripts/teamManagement.js
@@ -162,23 +162,31 @@ var teamData = {
 
 
 $(document).ready(function() {
-    var data = getData();
-    if (data != null)(displayData(data));
+    getData();
 });
 
 function getData() {
-    //Change to controller post
-    return teamData;
+    $.post('../../controller/teamManagement.php', { "action": "getData" }, function(data, status) { checkData(data, status) })
+}
+
+function checkData(data, status) {
+    if (status != "success") { alert("Connection to the DB can't be established, please try later."); return; }
+    console.log(JSON.parse(data));
+    data = JSON.parse(data);
+    if (Object.keys(data["teams"]).length == 0) { alert("No teams have been found, create a new team by clicking on the left!"); return; }
+    displayData(data);
 }
 
 function displayData(data) {
-    Object.values(data).forEach(val => {
+
+    Object.values(data["teams"]).forEach(team => {
+        var teamID = team["teamID"];
         var projects = "";
-        Object.values(val["assignedProjects"]).forEach(project => {
+        Object.values(data["projects"]).filter(e => e["assignedTeamID"] == teamID).forEach(project => {
             projects += `<div id="${project["projectID"]}" class="projectPod"><span>${project["projectName"]}</span><span class="d-none">${project["projectDescription"]}</span></div>`
         })
         var engineers = "";
-        Object.values(val["assignedEmployees"]).forEach(employee => {
+        Object.values(data["employees"]).filter(e => e["assignedTeam"] == teamID).forEach(employee => {
             engineers += `
             <div id="${employee["employeeID"]}" class="engineerPod">
                 <div class="d-inline-block">
@@ -187,29 +195,69 @@ function displayData(data) {
                 </div>
                 <div>
                     <button id="${employee["employeeID"]}-EDIT" class="BTN rounded-3"><i class="fa-solid fa-pen-to-square"></i></button>
-                     <button id="${employee["employeeID"]}-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
+                        <button id="${employee["employeeID"]}-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
                 </div>
             </div>
             `;
         })
-        console.log(val)
         $("#teamHolder")[0].innerHTML += `
-        <div class="teamContainer shadow rounded-3 p-2 d-inline-block" id="${val["teamID"]}">
+        <div class="teamContainer shadow rounded-3 p-2 d-inline-block" id="${teamID}">
             <div class="d-flex justify-content-between">
-                <input id="${val["teamName"]}" value="${val["teamName"]}" class="me-4"></input>
-                <button id="${val["teamID"]}-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
+                <input id="${team["teamName"]}" value="${team["teamName"]}" class="me-4"></input>
+                <button id="${teamID}-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
             </div>
             <div class="teamDivider mt-2 mb-2 ms-auto me-auto rounded-3"></div>
             <span class="ms-1">Projects</span>
-            <div id="${val["teamID"]}-ProjectsContainer p-4" class="projectContainer">
+            <div id="${teamID}-ProjectsContainer p-4" class="projectContainer">
                 ${projects}
             </div>
             <div class="teamDivider mt-2 mb-2 ms-auto me-auto rounded-3"></div>
             <span class="ms-1">Engineers</span>
-            <div id="${val["teamID"]}-EngineersContainer ms-1">
+            <div id="${teamID}-EngineersContainer ms-1">
                 ${engineers}
             </div>
         </div>
         `;
-    })
+    });
+
+
+    // Object.values(data).forEach(val => {
+    //     var projects = "";
+    //     Object.values(val["assignedProjects"]).forEach(project => {
+    //         projects += `<div id="${project["projectID"]}" class="projectPod"><span>${project["projectName"]}</span><span class="d-none">${project["projectDescription"]}</span></div>`
+    //     })
+    //     var engineers = "";
+    //     Object.values(val["assignedEmployees"]).forEach(employee => {
+    //         engineers += `
+    //         <div id="${employee["employeeID"]}" class="engineerPod">
+    //             <div class="d-inline-block">
+    //                 <span>${employee["employeeName"]}, </span>
+    //                 <span>${employee["employeeRole"]}</span>
+    //             </div>
+    //             <div>
+    //                 <button id="${employee["employeeID"]}-EDIT" class="BTN rounded-3"><i class="fa-solid fa-pen-to-square"></i></button>
+    //                  <button id="${employee["employeeID"]}-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
+    //             </div>
+    //         </div>
+    //         `;
+    //     })
+    //     $("#teamHolder")[0].innerHTML += `
+    //     <div class="teamContainer shadow rounded-3 p-2 d-inline-block" id="${val["teamID"]}">
+    //         <div class="d-flex justify-content-between">
+    //             <input id="${val["teamName"]}" value="${val["teamName"]}" class="me-4"></input>
+    //             <button id="${val["teamID"]}-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
+    //         </div>
+    //         <div class="teamDivider mt-2 mb-2 ms-auto me-auto rounded-3"></div>
+    //         <span class="ms-1">Projects</span>
+    //         <div id="${val["teamID"]}-ProjectsContainer p-4" class="projectContainer">
+    //             ${projects}
+    //         </div>
+    //         <div class="teamDivider mt-2 mb-2 ms-auto me-auto rounded-3"></div>
+    //         <span class="ms-1">Engineers</span>
+    //         <div id="${val["teamID"]}-EngineersContainer ms-1">
+    //             ${engineers}
+    //         </div>
+    //     </div>
+    //     `;
+    // })
 }
