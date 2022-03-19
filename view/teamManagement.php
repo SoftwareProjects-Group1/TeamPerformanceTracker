5f879d2951a219f3b03a6692fabfec2051ca937c
iv <91138367+Nox-iv@users.noreply.github.com>
2022-03-19 11:40:47 +0000

    teamManagementUIUpdate

--- view/teamManagement.php
+++ view/teamManagement.php
@@ -14,16 +14,64 @@
                 <button>Save Changes</button>
             </div>
             <div class="col-10">
-                <div class="h-100 w-100 mainBox p-4">
+                <div class="h-100 w-100 mainBox p-4 text-center">
                     <div class="teamContainer shadow rounded-3 p-2 d-inline-block" id="replaceWithTeamID">
                         <div class="d-flex justify-content-between">
                             <input id="teamName" value="Team Name" class="me-4"></input>
-                            <button id="replaceWithTeamID-DEL" class="delTeamBTN rounded-3"><i class="fa-solid fa-x"></i></button>
+                            <button id="replaceWithTeamID-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
                         </div>
                         <div class="teamDivider mt-2 mb-2 ms-auto me-auto rounded-3"></div>
                         <span class="ms-1">Projects</span>
-                        <div id="replacewithTeamID-ProjectsContainer">
-                            <div id="replaceWithProjectID"></div>
+                        <div id="replacewithTeamID-ProjectsContainer p-4" class="projectContainer">
+                            <div id="replaceWithProjectID" class="projectPod"><span>Test Project</span></div>
+                            <div id="replaceWithProjectID" class="projectPod"><span>Test Project two</span></div>
+                            <div id="replaceWithProjectID" class="projectPod"><span>Test Project three</span></div>
+                            <div id="replaceWithProjectID" class="projectPod"><span>Test Project four</span></div>
+                            <div id="replaceWithProjectID" class="projectPod"><span>Test Project 5</span></div>
+                        </div>
+                        <div class="teamDivider mt-2 mb-2 ms-auto me-auto rounded-3"></div>
+                        <span class="ms-1">Engineers</span>
+                        <div id="replacewithTeamID-EngineersContainer ms-1">
+                            <div id="replaceWithEngineerID" class="engineerPod">
+                                <div class="d-inline-block">
+                                    <span>James Smith, </span>
+                                    <span>Head Engineer</span>
+                                </div>
+                                <div>
+                                    <button id="replaceWithEngineerID-DEL" class="BTN rounded-3"><i class="fa-solid fa-pen-to-square"></i></button>
+                                    <button id="replaceWithEngineerID-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
+                                </div>
+                            </div>
+                            <div id="replaceWithEngineerID" class="engineerPod">
+                                <div class="d-inline-block">
+                                    <span>Adam Jones, </span>
+                                    <span>Senior Engineer</span>
+                                </div>
+                                <div>
+                                    <button id="replaceWithEngineerID-DEL" class="BTN rounded-3"><i class="fa-solid fa-pen-to-square"></i></button>
+                                    <button id="replaceWithEngineerID-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
+                                </div>
+                            </div>
+                            <div id="replaceWithEngineerID" class="engineerPod">
+                                <div class="d-inline-block">
+                                    <span>Sarah Owens, </span>
+                                    <span>Engineer</span>
+                                </div>
+                                <div>
+                                    <button id="replaceWithEngineerID-DEL" class="BTN rounded-3"><i class="fa-solid fa-pen-to-square"></i></button>
+                                    <button id="replaceWithEngineerID-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
+                                </div>
+                            </div>
+                            <div id="replaceWithEngineerID" class="engineerPod">
+                                <div class="d-inline-block">
+                                    <span>Alex Jives, </span>
+                                    <span>Junior Engineer</span>
+                                </div>
+                                <div>
+                                    <button id="replaceWithEngineerID-DEL" class="BTN rounded-3"><i class="fa-solid fa-pen-to-square"></i></button>
+                                    <button id="replaceWithEngineerID-DEL" class="BTN rounded-3"><i class="fa-solid fa-x"></i></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
