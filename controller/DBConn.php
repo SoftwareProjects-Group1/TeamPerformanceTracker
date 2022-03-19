19daae6058bba671efdd4adc68823196db751c54
iv <91138367+Nox-iv@users.noreply.github.com>
2022-03-19 18:22:06 +0000

    dbConnUpdate

--- controller/DBConn.php
+++ controller/DBConn.php
@@ -10,10 +10,11 @@ if(!isset($GLOBALS['DBConn'])){
         connect();
     }
 }
-
 function connect() {
     try {
         $m = new MongoDB\Driver\Manager('mongodb+srv://group1:fvAIyyCRp4PBaDPQ@clst01.to6hh.mongodb.net/projectDB?retryWrites=true&w=majority');
+        $listdatabases = new MongoDB\Driver\Command(["listCollections" => 1]);
+        $res = $m->executeCommand("projectDB", $listdatabases);
         $GLOBALS['DBConnStatus'] = true;
         $GLOBALS['DBConn'] = $m;
     }
