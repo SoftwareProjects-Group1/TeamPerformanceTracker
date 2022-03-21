22d735f0c7d49bf710caf03b546e4e5b44db0e70
JAMMY HALL <jameshall8@outlook.com>
2022-03-21 17:36:21 +0000

    Update project now returns

--- view/ViewProject.php
+++ view/ViewProject.php
@@ -15,7 +15,7 @@
              <td>Project Description</td>
                <td>Project Budget</td>
                <td>Project Manager</td>
-                                                
+                    <td> Action <td>                       
                  </thead>  ";     
         $results=[];
         $q = new MongoDB\Driver\Query([],[]);
@@ -27,7 +27,9 @@
             echo "<td>" . $row->projectDescription . "</td>"; 
             echo "<td>" . $row->projectBudget. "</td>"; 
             echo "<td>" . $row->ProjectManager. "</td>"; 
-            //echo "<td>" . $row->assignedTeamID . "</td>"; 
+            //echo "<td>" . $row->assignedTeamID . "</td>";
+            echo '<td><a class="btn btn-info" href="updateProject.php?ProjectName='. $row->projectName.'">Update</a></td>';
+
 
          echo "</tr>";
         
@@ -38,25 +40,24 @@
         
 }
 
+       
         ?>
         
 
-      
-
-
-
 <div class="main">    
+    
     <div class="inner_main">
+
+    
+    
         <div class="container">
+
             <div class="row">
                 <div class="col-sm-9 col-md-7 col-lg-12 mx-auto">
                     <div class="card border-0 shadow rounded-3 my-5">
                         <div class="card-body p-4 p-5">
-                            <main role="main" class="pb-3">
-                                <h2 class = "title" style = "padding-top:25px">View Projects</h2><br>
-                                <div class="row">
-                                    <div class="col-10">
-                                                   
+
+                                                               
                                                      
                                                 
                                                 <?php
@@ -68,8 +69,10 @@
                                                       
                                         </table>    
                                     </div>
+                                    <a href="CreateProject.php"  type="button" class="btn btn-primary">Create New Project</a>	     
+
                                 </div>
-                                <a href="CreateProject.php"  type="button" class="btn btn-primary">Create New Project</a>	                            
+                         
                             </main>
                         </div>
                     </div>
---
+++ view/updateProject.php
@@ -0,0 +1,144 @@
+<?php
+    require("../view/_inc/head.php");
+    require("../view/_inc/header.php");
+
+
+    function getData(){
+        
+        $m = new MongoDB\Driver\Manager('mongodb+srv://group1:fvAIyyCRp4PBaDPQ@clst01.to6hh.mongodb.net/projectDB?retryWrites=true&w=majority');
+        $filter = [ 'projectName' => $_GET['ProjectName']]; 
+
+        $query = new MongoDB\Driver\Query($filter);     
+        $res = $m->executeQuery("projectDB.Projects", $query);
+
+        $pro = current($res->toArray());
+
+
+        print_r($pro);
+    }
+
+
+    function update(){
+      $num = rand(1000,10000);
+      
+      $bulk = new MongoDB\Driver\BulkWrite;
+
+      $document2 = ['projectID' => $num, 'projectName' => $_POST['name'], 'projectDescription' => $_POST['description'], 'projectBudget' => $_POST['budget'], 'ProjectManager' => $_POST['EmpStatus']];
+
+      $_id3 = $bulk->insert($document2);
+
+      var_dump($_id3);
+
+      $m = new MongoDB\Driver\Manager('mongodb+srv://group1:fvAIyyCRp4PBaDPQ@clst01.to6hh.mongodb.net/projectDB?retryWrites=true&w=majority');
+      $result = $m->executeBulkWrite('projectDB.Projects', $bulk);
+
+    }
+    $allFields = "yes";
+    $errName = $errBudget = $errDesc = $errManager = "";
+
+
+
+    if (isset($_POST["submit"])){
+
+      if ($_POST['name']==""){
+          $errName = "This field is mandatory";
+          $allFields = "no";
+      }
+      if ($_POST['budget']==null){
+          $errBudget = "This field is mandatory";
+          $allFields = "no";
+      }
+      if ($_POST['description']==""){
+          $errDesc = "This field is mandatory";
+          $allFields = "no";
+      }
+      if ($_POST['EmpStatus']==""){
+        $errManager = "This field is mandatory";
+        $allFields = "no";
+    }
+      
+      
+  
+      if($allFields == "yes")
+      {
+        insert();
+      }
+  }
+?>
+
+         
+
+
+<div class="main">    
+    <div class="inner_main">
+    <section class="vh-100">
+<body>
+
+<?php getData(); ?>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="card border-0 shadow rounded-3 my-5">
+        <div class="card-body p-4 p-sm-5">
+          <h5 class="card-title text-center mb-5 fw-light fs-5">Update</h5>
+
+          <form method="post">
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="projectName" placeholder="Project Name" name="name">
+              <label for="floatingInput">Project Name</label>
+              <span class="text-danger"><?php echo $errName; ?></span>
+
+            </div>
+
+            <div class="form-floating mb-3">                    
+              <select class="select form-control" name="EmpStatus">
+                <option value="0" disabled>Select Team</option>
+                <option value="1">Manager 1</option>
+                <option value="2">Manager 2</option>
+                <option value="3">Manager 3</option>
+                <option value="4">Manager 4</option>
+              </select>
+              <span class="text-danger"><?php echo $errDesc; ?></span>
+
+
+              <?php?>
+              <label class="form-label select-label">Select Project Manager</label>
+            </div>    
+            
+            <div class="form-floating mb-3">
+              <input type="number" class="form-control" id="projectBudget" placeholder="Budget" name="budget">
+              <label for="floatingPassword">Budget</label>
+              <span class="text-danger"><?php echo $errBudget; ?></span>
+
+            </div>
+
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="projectDescription" placeholder="Project Name" name="description">
+              <label for="floatingInput">Project Description</label>
+              <span class="text-danger"><?php echo $errDesc; ?></span>
+
+            </div>
+            
+            <div class="d-grid text-center">
+              <button class="  btn btn-primary btn-login text-uppercase fw-bold" type="submit" name="submit">Update</button>
+            </div><br>
+
+            <div class="d-grid text-center">
+              <a href="index.php" class="btn btn-secondary btn-login text-uppercase fw-bold" type="submit" >Return to home</a>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</div>
+</div>
+</section>
+
+<?php
+    require("../view/_inc/footer.php");
+?>
